120167b8535b02d513267c90e74b6463da9b0a99
harryruto <lambert.clement1@outlook.fr>
2026-07-18 12:11:25 +0200

    Rename functions and variables in math.h

--- math.h
+++ math.h
@@ -1,15 +1,15 @@
 #ifndef MATH_H_INCLUDED
 #define MATH_H_INCLUDED
 
-double addition(double nombre1, double nombre2);
-double soustraction(double nombre1, double nombre2);
-double multiplication(double nombre1, double nombre2);
-double division(double nombre1, double nombre2);
-double carre(double nombre1);
-double puissance(double nombre1, double puissance);
-double aireCR(double longueur, double largeur);
-double aireT(double longueur, double largeur);
-
+double addition(double number1, double number2);
+double subtraction(double number1, double number2);
+double multiplication(double number1, double number2);
+double division(double number1, double number2);
+double square(double number1);
+double power(double number1, double exponent);
+double areaSR(double length, double width);
+double areaT(double length, double width);
+double squareRoot(double number);
 
 #endif // MATH_H_INCLUDED
 
