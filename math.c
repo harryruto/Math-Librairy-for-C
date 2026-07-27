6e8abaedeeb11b859f4180663598f4fe0918c3ce
harryruto <lambert.clement1@outlook.fr>
2026-07-27 17:44:52 +0200

    Refactor squareRoot and remove unused functions
    Refactor squareRoot function to improve accuracy and handle edge cases.

--- math.c
+++ math.c
@@ -1,35 +1,3 @@
-#include "math.h"
-
-double addition(double number1, double number2) {
-    return number1 + number2;
-}
-
-double subtraction(double number1, double number2) {
-    return number1 - number2;
-}
-
-double multiplication(double number1, double number2) {
-    return number1 * number2;
-}
-
-double division(double number1, double number2) {
-    return number1 / number2;
-}
-
-double square(double number1) {
-    return number1 * number1;
-}
-
-double power(double number1, double exponent) {
-    double startingNumber = number1;
-    double currentPower = 0;
-
-    while (currentPower != exponent - 1) {
-        currentPower++;
-        number1 = number1 * startingNumber;
-    }
-    return number1;
-}
 
 double areaSR(double length, double width) {
     return length * width;
@@ -40,16 +8,27 @@ double areaT(double length, double width) {
 }
 
 double squareRoot(double number) {
-    double testedNumber = 1;
-    double obtainedNumber = 0;
-
-    while (obtainedNumber != number) {
-        testedNumber++;
-        obtainedNumber = square(testedNumber);
-    if (obtainedNumber > number) {
+    if(number <  0)
+    {
         return -1;
     }
-}
+    if(number == 0) {
+        return 0;
+    }
+        double x = number;
+    double tolerance = 0.00001;
 
-    return testedNumber;
+    while(1) {
+        double nextX = 0.5 * (x + number / x);
+
+        double diff = nextX - x;
+
+        if(diff < 0) {
+            diff = -diff;
+            }
+        if(diff < tolerance) {
+        return nextX;
+        }
+        x = nextX;
+    }
 }
