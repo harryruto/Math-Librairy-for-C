aa122eab80830100a14c078e51c967c156426f66
harryruto <lambert.clement1@outlook.fr>
2026-08-03 15:26:30 +0200

    Rename math functions for consistency

--- math.h
+++ math.h
@@ -1,15 +1,15 @@
 #ifndef MATH_H_INCLUDED
 #define MATH_H_INCLUDED
 
-double addition(double number1, double number2);
-double subtraction(double number1, double number2);
-double multiplication(double number1, double number2);
-double division(double number1, double number2);
-double square(double number1);
-double power(double number1, double exponent);
-double areaSR(double length, double width);
-double areaT(double length, double width);
-double squareRoot(double number);
+double mthadd(double number1, double number2);
+double mthsub(double number1, double number2);
+double mthmult(double number1, double number2);
+double mthdivd(double number1, double number2);
+double mthsq(double number1);
+double mthp(double number1, double exponent);
+double mtharSR(double length, double width);
+double mtharT(double length, double width);
+double mthsqr(double number);
 
 #endif // MATH_H_INCLUDED
 
